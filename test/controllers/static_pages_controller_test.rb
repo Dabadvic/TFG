53d9230f76a38a77a73ef65adfb1bdf52f7b9a50
David Abad Vich <davidabad10@gmail.com>
2015-10-22 09:34:09 +0200

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Proyecto"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Proyecto"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "About | Proyecto"
   end
 
 end
